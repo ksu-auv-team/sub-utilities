215b83927548c1449b04e49b92caba8249dfe74b
Daniel Castle <dcastle4@students.kennesaw.edu>
2019-10-31 21:50:14 -0400

    i tried

--- scripts/runsub.py
+++ scripts/runsub.py
@@ -32,6 +32,23 @@ class SubSession():
         killswitch_start_sub = rospy.Subscriber("killswitch_run_start", Bool, self.killswitch_start_callback)
         killswitch_realtime_sub = rospy.Subscriber("killswitch_is_killed", Bool, self.killswitch_realtime_callback, queue_size=1)
         
+    def create_args(self):
+		# Parse command line arguments:
+		parser = argparse.ArgumentParser(description="run the submarine")
+		parser.add_argument('-i', '--internet-address', help="override default hostname or ip address for remote computer (not currently functional)")
+		parser.add_argument('-m', '--manual', action='store_true', help="Will not run state machine")
+		parser.add_argument('-s', '--state-machine', default="QualifyStraightMachine", help="set name of state machine to use (default: %(default)s)")
+		parser.add_argument('-n', '--network-model', default="qual_2_rcnn_frozen", help="set name of neural network to use (default: %(default)s)")
+		parser.add_argument('-v', '--verbosity', help="set logging verbosity (doesn't work)")
+		parser.add_argument('--no-arduino', action='store_true', help='Runs the sub without running any physical arduino hardware.')
+		parser.add_argument('--no-network', action='store_true', help='Runs the sub without running the neural network')
+		parser.add_argument('--no-save-images', action='store_const', default ='', const='--no-save-images', help='Will not record any video/pictures from the sub')
+		parser.add_argument('--debug-execute', action='store_const', default='', const='--debug', help='Will run execute with the debug flag')
+		parser.add_argument('--start-front-network', action='store_true', help='Will begin with the front neural network running')
+		parser.add_argument('--start-bottom-network', action='store_true', help='Will begin with the bottom neural network running')
+		return parser.parse_args()
+		
+		
     # shut down child processes for restarting them cleanly or exiting
     def kill_children(self):
         self.curr_children
@@ -71,7 +88,7 @@ class SubSession():
             rc = subprocess.Popen(roscore_command, stdout=rcout, stderr=rcout)
             return rc
 
-    def start_video(self):
+    def start_video(self, args):
         video_string = "python " + self.script_directory + "camera_node.py " + args.no_save_images
         video_command = video_string.split()
 
@@ -80,7 +97,7 @@ class SubSession():
             video = subprocess.Popen(video_command, stdout=videoout, stderr=videoout)
             return video
 
-    def start_network(self):
+    def start_network(self, args):
         network_string = "python3 " + self.script_directory + '../submodules/jetson_nano_inference/jetson_live_object_detection.py --model ' + args.network_model + ' ' + args.no_save_images
         network_command = network_string.split()
     
@@ -98,7 +115,7 @@ class SubSession():
             mv = subprocess.Popen(movement_command, stdout=mvout, stderr=mvout)
             return mv 
 
-    def start_execute(self):
+    def start_execute(self, args):
         execute_string = 'python ' + self.script_directory + '../submodules/subdriver/execute_withState.py --machine ' + args.state_machine + ' ' + args.debug_execute
         execute_command = execute_string.split()
 
@@ -119,7 +136,7 @@ class SubSession():
     #start ALL the things
     def start(self):     
         # Run the Video Node
-        self.curr_children.append(self.start_video())
+        self.curr_children.append(self.start_video(args))
         
         self.delay_start = time.time() # The time we will compare our arduino time to
         while(time.time() - self.delay_start < 2 and not self.sub_is_killed):
@@ -136,7 +153,7 @@ class SubSession():
         if(args.manual):
             print('Manual Mode enabled, start your joystick node')
         else:
-            self.curr_children.append(self.start_execute())
+            self.curr_children.append(self.start_execute(args))
              
         print('exiting start')
 
@@ -160,23 +177,12 @@ class SubSession():
         
 
 if __name__ == '__main__':
-    # Parse command line arguments:
-    parser = argparse.ArgumentParser(description="run the submarine")
-    parser.add_argument('-i', '--internet-address', help="override default hostname or ip address for remote computer (not currently functional)")
-    parser.add_argument('-m', '--manual', action='store_true', help="Will not run state machine")
-    parser.add_argument('-s', '--state-machine', default="QualifyStraightMachine", help="set name of state machine to use (default: %(default)s)")
-    parser.add_argument('-n', '--network-model', default="qual_2_rcnn_frozen", help="set name of neural network to use (default: %(default)s)")
-    parser.add_argument('-v', '--verbosity', help="set logging verbosity (doesn't work)")
-    parser.add_argument('--no-arduino', action='store_true', help='Runs the sub without running any physical arduino hardware.')
-    parser.add_argument('--no-network', action='store_true', help='Runs the sub without running the neural network')
-    parser.add_argument('--no-save-images', action='store_const', default ='', const='--no-save-images', help='Will not record any video/pictures from the sub')
-    parser.add_argument('--debug-execute', action='store_const', default='', const='--debug', help='Will run execute with the debug flag')
-    parser.add_argument('--start-front-network', action='store_true', help='Will begin with the front neural network running')
-    parser.add_argument('--start-bottom-network', action='store_true', help='Will begin with the bottom neural network running')
-    args = parser.parse_args()
+	
+	# Create args
+	args = self.create_args()
 
     # Wait for arduino to start
-    time.sleep(3)
+    #time.sleep(3)
 
     # Create Subsession
     go_sub_go = SubSession(args.no_arduino)
@@ -192,13 +198,13 @@ if __name__ == '__main__':
             bottom_pub = rospy.Publisher('enable_bottom_network', Bool, queue_size=1)
             bottom_pub.publish(True)
 
-    # captureing Ctrl+C
+    # capturing Ctrl+C
     signal.signal(signal.SIGINT, go_sub_go.signal_handler)
     
     # If we are running without an arduino hooked up, just run the start, don't listen()
     if args.no_arduino:
         if(not args.no_network):
-            go_sub_go.startup_processes.append(go_sub_go.start_network())
+            go_sub_go.startup_processes.append(go_sub_go.start_network(args))
         time.sleep(3)
         go_sub_go.start()
 
@@ -206,7 +212,7 @@ if __name__ == '__main__':
     else:
         go_sub_go.startup_processes.append(go_sub_go.start_arduino())
         if(not args.no_network):
-            go_sub_go.startup_processes.append(go_sub_go.start_network())
+            go_sub_go.startup_processes.append(go_sub_go.start_network(args))
 
     #the loop everything runs from
     rospy.spin()
