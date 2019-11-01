5441b75dc6b6e58e2fe69d2840fd9d9d3acbbc3a
DylanCrawford <dylancstudent@gmail.com>
2019-11-01 15:02:02 -0400

    I tried to fix the runsub errors. maybe broken

--- scripts/runsub.py
+++ scripts/runsub.py
@@ -12,7 +12,7 @@ from std_msgs.msg import Bool
 from colorama import Fore
 
 class SubSession():
-    def __init__(self, no_arduino=False):
+    def __init__(self, no_arduino=False, no_save_images='', network_model = "qual_2_rcnn_frozen", state_machine ="QualifyStraightMachine", debug_execute = '', manual = False):
         # Subprocesses:
         self.curr_children = []
         self.startup_processes = []
@@ -20,7 +20,6 @@ class SubSession():
         # Arduino variables
         self.delay_start = 0
         self.sub_is_killed = True
-        self.no_arduino = no_arduino
 
         #keep logs from each start in a separate directory
         self.script_directory = os.path.dirname(os.path.realpath(__file__)) + '/'
@@ -72,7 +71,7 @@ class SubSession():
             return rc
 
     def start_video(self):
-        video_string = "python " + self.script_directory + "camera_node.py " + args.no_save_images
+        video_string = "python " + self.script_directory + "camera_node.py " + no_save_images
         video_command = video_string.split()
 
         print(Fore.GREEN + "starting video node with command: " + Fore.WHITE + video_string)
@@ -81,7 +80,7 @@ class SubSession():
             return video
 
     def start_network(self):
-        network_string = "python3 " + self.script_directory + '../submodules/jetson_nano_inference/jetson_live_object_detection.py --model ' + args.network_model + ' ' + args.no_save_images
+        network_string = "python3 " + self.script_directory + '../submodules/jetson_nano_inference/jetson_live_object_detection.py --model ' + network_model + ' ' + no_save_images
         network_command = network_string.split()
     
         print(Fore.GREEN + 'starting Neural Network with command: ' + Fore.WHITE + network_string)
@@ -99,7 +98,7 @@ class SubSession():
             return mv 
 
     def start_execute(self):
-        execute_string = 'python ' + self.script_directory + '../submodules/subdriver/execute_withState.py --machine ' + args.state_machine + ' ' + args.debug_execute
+        execute_string = 'python ' + self.script_directory + '../submodules/subdriver/execute_withState.py --machine ' + state_machine + ' ' + debug_execute
         execute_command = execute_string.split()
 
         print(Fore.GREEN + 'starting execute with command: ' + Fore.WHITE + execute_string)
@@ -133,7 +132,7 @@ class SubSession():
             pass
 
         # Run Execute
-        if(args.manual):
+        if(manual):
             print('Manual Mode enabled, start your joystick node')
         else:
             self.curr_children.append(self.start_execute())
